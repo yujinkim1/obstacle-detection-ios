513a6c87be97e77637f7afdd49ec9cf50f53dc8a
yujinkim1 <yujin7621@me.com>
2023-05-01 15:44:32 +0900

    Initial Commit

---
+++ ObstacleDetection/ObstacleDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObstacleDetectionApp.swift
+//  ObstacleDetection
+//
+//  Created by Yujin Kim on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct ObstacleDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
